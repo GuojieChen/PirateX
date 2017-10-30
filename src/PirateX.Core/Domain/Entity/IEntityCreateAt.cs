4eb6a383a1485cc000f31e33afe0df828e39869f
Guojie Chen <chenguojie0408@gmail.com>
2017-10-30 11:14:48 +0800

    时间字段

---
+++ src/PirateX.Core/Domain/Entity/IEntityCreateAt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirateX.Core.Domain.Entity
+{
+    public interface IEntityCreateAt
+    {
+        long CreateAt { get; set; }
+
+        DateTime CreateAtAsDateTime { get; }
+    }
+}
--- src/PirateX.Core/Utils/TimeUtil.cs
+++ src/PirateX.Core/Utils/TimeUtil.cs
@@ -13,5 +13,17 @@ namespace PirateX.Core.Utils
         {
             return (dateTime.ToUniversalTime().Ticks - DateTime.Parse("1970-01-01 00:00:00").Ticks) / 10000;
         }
+
+        /// <summary>
+        /// 时间戳（毫秒级）转化成UTC时间
+        /// </summary>
+        /// <param name="timestamp"></param>
+        /// <returns></returns>
+        public static DateTime ConvertToDateTime(this long timestamp)
+        {
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddMilliseconds(timestamp).ToLocalTime();
+            return dtDateTime;
+        }
     }
 }
