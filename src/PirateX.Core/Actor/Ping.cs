f11ef8d63ae89a81ebf7431cbc3988e393344c82
Guojie Chen <chenguojie0408@gmail.com>
2018-01-18 16:49:34 +0800

    ping返回当前服务器时间

--- src/PirateX.Core/Actor/Ping.cs
+++ src/PirateX.Core/Actor/Ping.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using PirateX.Core.Utils;
+using ProtoBuf;
 
 namespace PirateX.Core.Actor
 {
     /// <summary>
     /// ping
     /// </summary>
-    public class Ping : RepAction
+    public class Ping : RepAction<PingResponse>
     {
-        public override void Execute()
+        public override PingResponse Play()
         {
-
+            return new PingResponse()
+            {
+                Now = DateTime.UtcNow.FromDateTime()
+            };
         }
+    }
+
+    [ProtoContract]
+    public class PingResponse
+    {
+        /// <summary>
+        /// 服务器当前时间 UTC
+        /// </summary>
+        [ProtoMember(1)]
+        public string Now { get; set; }
     }
 }
