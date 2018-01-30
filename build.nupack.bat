2935c36aa9f8967c3e3a83eae8659200c0250436
Guojie Chen <chenguojie0408@gmail.com>
2018-01-30 13:49:32 +0800

    string to array  兼容

--- build.nupack.bat
+++ build.nupack.bat
@@ -1,4 +1,7 @@
 nuget pack %cd%\src\PirateX.Core\PirateX.Core.csproj
+nuget pack %cd%\src\PirateX.Net.NetMQ\PirateX.Net.NetMQ.csproj
 nuget pack %cd%\src\PirateX.Net.SuperSocket\PirateX.Net.SuperSocket.csproj
 nuget pack %cd%\src\PirateX.Protocol\PirateX.Protocol.csproj
+nuget pack %cd%\src\PirateX.ServiceStackV3\PirateX.ServiceStackV3.csproj
+nuget pack %cd%\src\PirateX.Middleware\PirateX.Middleware.csproj
 pause
