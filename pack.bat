8f5d48c01dbcc3fc805a0da123bf8385ec8d991b
Guojie Chen <chenguojie0408@gmail.com>
2018-01-30 16:26:54 +0800

    打包和上传的脚本统一到一个文件 添加ico

---
+++ pack.bat
@@ -0,0 +1,21 @@
+@echo off
+set version=1.0.2
+set OutputDirectory=bin\packages\%version%
+
+if not exist %OutputDirectory% mkdir %OutputDirectory%
+
+echo "nuget pack"
+
+nuget pack %cd%\src\PirateX.Core\PirateX.Core.csproj -Properties -IncludeReferencedProjects -OutputDirectory %OutputDirectory% -version %version%
+nuget pack %cd%\src\PirateX.Net.NetMQ\PirateX.Net.NetMQ.csproj  -Properties -IncludeReferencedProjects -OutputDirectory %OutputDirectory% -version %version%
+nuget pack %cd%\src\PirateX.Net.SuperSocket\PirateX.Net.SuperSocket.csproj  -Properties -IncludeReferencedProjects -OutputDirectory %OutputDirectory% -version %version%
+nuget pack %cd%\src\PirateX.Protocol\PirateX.Protocol.csproj  -Properties -IncludeReferencedProjects -OutputDirectory %OutputDirectory% -version %version%
+nuget pack %cd%\src\PirateX.ServiceStackV3\PirateX.ServiceStackV3.csproj  -Properties -IncludeReferencedProjects -OutputDirectory %OutputDirectory% -version %version%
+nuget pack %cd%\src\PirateX.Middleware\PirateX.Middleware.csproj  -Properties -IncludeReferencedProjects -OutputDirectory %OutputDirectory% -version %version%
+
+
+echo "push to ProGet"
+
+NuGet.exe push %OutputDirectory%\* Admin:Admin -Source http://192.168.1.158:8001/nuget/mrglee/
+
+pause
--- push.package.bat
+++
@@ -1,3 +0,0 @@
-NuGet.exe push .\bin\* Admin:Admin -Source http://192.168.1.158:8001/nuget/mrglee/
-
-pause
